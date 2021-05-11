739422f29be0a79c8df3fb590128b424f6b05691
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-11 16:47:42 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Mike is a ............... student. He always gets good grades.",
+        options:["hard-working", "elementary", "standart", "lazy", "good-looking"],
+        answer: 0
+    },
+    {
+        q:"He crashed his car into a tree. It was badly ................    .",
+        options:["hopeless", "damaged", "hurt", "wounded", "injured"],
+        answer: 1
+    },
+    {
+        q:"It was a ............... question. I couldn't understand a word of it.",
+        options:["necessary", "unnecessary", "difficult", "easy", "essential"],
+        answer: 2
+    },
+    {
+        q:"Thank you for the dinner last night. I enjoyed it very much. It was ...............      .",
+        options:["experienced", "excited", "expensive", "excellent", "extreme"],
+        answer: 3
+    },
+    {
+        q:"He suddenly came face to face with two ............... men and started to run away.",
+        options:["lonely", "legless", "unlucky", "athletic", "armed"],
+        answer: 4
+    },
+    {
+        q:"I sat through all those cold and ............... nights, just thinking of you.",
+        options:["lonely", "injured", "selfish", "daily", "certain"],
+        answer: 0
+    },
+    {
+     q:"Stay away from that park after dark. It's too ...............     .",
+        options:["nervous", "dangerous", "stormy", "blind", "large"],
+        answer: 1   
+    },
+    {
+        q:"Believe me I feel truly ............... for your great kindness. How can I ever repay you?",
+        options:["cheerful", "thoughtful", "grateful", "beatiful", "shameful"],
+        answer: 2
+    },
+    {
+        q:"You can't expect every ............... man to become a computer specialist.",
+        options:["economic", "rare", "effective", "average", "interesting"],
+        answer: 3
+    },
+    {
+        q:"You don't know this? How ............... you are!",
+        options:["foreign", "complicated", "ungrateful", "opposite", "ignorant"],
+        answer: 4
+    }
+]
